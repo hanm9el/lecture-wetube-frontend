1161e4c1f08c23cd833856269801316a2cda21da
HYUN <hanm9el@naver.com>
2025-12-29 16:49:43 +0900

    axios + check -nickname  통신완료

---
+++ src/components/ui/Input.tsx
@@ -0,0 +1,33 @@
+import type { InputHTMLAttributes } from "react";
+import type { UseFormRegisterReturn } from "react-hook-form";
+import { twMerge } from "tailwind-merge";
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+    label?: string;
+    registration?: Partial<UseFormRegisterReturn>;
+    error?: string;
+    labelClassName?: string;
+}
+
+function Input({ label, registration, error, className, labelClassName, ...props }: InputProps) {
+    return (
+        <div className={twMerge(["w-full", "flex", "flex-col", "gap-1"])}>
+            {label && <label className={twMerge(["text-sm", "font-medium"], labelClassName)}>{label}</label>}
+            <input
+                className={twMerge(
+                    ["w-full", "px-3", "py-2"],
+                    ["text-sm", "text-text-default", "placeholder:text-text-disabled"],
+                    ["border-divider", "border", "rounded-md", "bg-background-default"],
+                    ["focus:outline-none", "focus:border-secondary-main"],
+                    error && ["border-error-main", "focus:border-error-main"],
+                    className,
+                )}
+                {...registration}
+                {...props}
+            />
+            {error && <span className={twMerge(["text-xs", "text-error-main"])}>{error}</span>}
+        </div>
+    );
+}
+
+export default Input;
---
+++ src/store/useThemeStore.ts
@@ -0,0 +1,22 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+interface ThemeState {
+    theme: "light" | "dark";
+    toggleTheme: VoidFunction; // 매개변수 없이, toggle만 업데이트하는 함수
+}
+
+export const useThemeStore = create<ThemeState>()(
+    persist(
+        set => ({
+            theme: "light",
+            // toggleTheme를 실행하면, 이 zustand가 관리하는 값을 변경할건데
+            // 지금 현재 값(state) 중 theme의 값이 light라면, dark로. light가 아니라면 light로 변경
+            toggleTheme: () =>
+                set(state => ({
+                    theme: state.theme === "light" ? "dark" : "light",
+                })),
+        }),
+        { name: "wetube-theme" },
+    ),
+);
