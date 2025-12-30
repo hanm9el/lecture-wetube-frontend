f81fd3740d19b84b30629878bea2fe0e23fadf63
HYUN <hanm9el@naver.com>
2025-12-30 13:05:08 +0900

    LoginRequiredModal 완성

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,35 @@
+import { type MouseEvent } from "react";
 import { twMerge } from "tailwind-merge";
 import { Link } from "react-router";
-import { MdCampaign, MdDarkMode, MdLightMode, MdMenu, MdSearch, MdVideoCall } from "react-icons/md";
+import {
+    MdCampaign,
+    MdDarkMode,
+    MdLightMode,
+    MdMenu,
+    MdSearch,
+    MdVideoCall,
+} from "react-icons/md";
 import { FaRegUserCircle, FaYoutube } from "react-icons/fa";
 import { useThemeStore } from "../store/useThemeStore.ts";
+import { useModalStore } from "../store/useModalStore.ts";
+import { useAuthStore } from "../store/useAuthStore.ts";
 
 function Header() {
     const { theme, toggleTheme } = useThemeStore();
+    const { openModal } = useModalStore();
+    const { isLoggedIn } = useAuthStore();
+
+    // MouseEvent 라는 이름의 타입이 Javascript에도 있고 React에도 있음
+    // 우리가 써야 하는건, React의 MouseEvent 타입이라 이를 명시적으로 수동으로 적어줘야 함
+    const handleUploadClick = (event: MouseEvent<HTMLAnchorElement>) => {
+        // 비회원이 누르면, 모달을 띄우고 끝내는 함수
+        if (isLoggedIn) {
+            // a 태그에 onClick을 사용하고 있기 때문에 a의 기본 기능인 "이동"을 막을 필요가 있음
+            // 이벤트 버블링 : 클릭 이벤트 등의 이벤트가 상위 요소로 전파되는 현상
+            event.preventDefault();
+            openModal("LOGIN_REQUIRED");
+        }
+    };
 
     return (
         <header
@@ -14,35 +38,71 @@ function Header() {
                 ["border-b", "border-divider"],
                 ["flex", "justify-between", "items-center"],
                 ["z-50"],
-            )}>
+            )}
+        >
             {/* 1. 왼쪽 */}
             <div className={twMerge(["flex", "items-center", "gap-4"])}>
-                <button className={twMerge(["p-2", "rounded-full", "hover:bg-text-default/10"])}>
+                <button
+                    className={twMerge([
+                        "p-2",
+                        "rounded-full",
+                        "hover:bg-text-default/10",
+                    ])}
+                >
                     <MdMenu className={twMerge(["w-6", "h-6"])} />
                 </button>
-                <Link to={"/"} className={twMerge(["flex", "items-center", "gap-2"])}>
-                    <FaYoutube className={twMerge(["w-8", "h-8", "text-primary-main"])} />
-                    <span className={twMerge(["text-xl", "font-bold"])}>WeTube</span>
+                <Link
+                    to={"/"}
+                    className={twMerge(["flex", "items-center", "gap-2"])}
+                >
+                    <FaYoutube
+                        className={twMerge(["w-8", "h-8", "text-primary-main"])}
+                    />
+                    <span className={twMerge(["text-xl", "font-bold"])}>
+                        WeTube
+                    </span>
                 </Link>
             </div>
 
             {/* 2. 중간 */}
-            <div className={twMerge(["flex-1", "max-w-[600px]"], ["flex", "items-center"])}>
+            <div
+                className={twMerge(
+                    ["flex-1", "max-w-[600px]"],
+                    ["flex", "items-center"],
+                )}
+            >
                 <div className={twMerge(["flex", "w-full"])}>
                     <input
                         placeholder={"검색"}
                         className={twMerge(
                             ["w-full", "px-4", "py-2"],
-                            ["text-text-default", "placeholder:text-text-disabled"],
-                            ["border", "border-divider", "rounded-l-full", "shadow-inner"],
-                            ["focus:outline-none", "focus:border-secondary-main"],
+                            [
+                                "text-text-default",
+                                "placeholder:text-text-disabled",
+                            ],
+                            [
+                                "border",
+                                "border-divider",
+                                "rounded-l-full",
+                                "shadow-inner",
+                            ],
+                            [
+                                "focus:outline-none",
+                                "focus:border-secondary-main",
+                            ],
                         )}
                     />
                     <button
                         className={twMerge(
                             ["px-4", "py-2"],
-                            ["border", "border-l-0", "rounded-r-full", "border-divider"],
-                        )}>
+                            [
+                                "border",
+                                "border-l-0",
+                                "rounded-r-full",
+                                "border-divider",
+                            ],
+                        )}
+                    >
                         <MdSearch className={twMerge(["w-6", "h-6"])} />
                     </button>
                 </div>
@@ -56,7 +116,8 @@ function Header() {
                         ["flex", "items-center", "justify-center", "p-2"],
                         ["rounded-full", "hover:bg-text-default/10"],
                     )}
-                    title={"공지사항"}>
+                    title={"공지사항"}
+                >
                     <MdCampaign className={twMerge(["w-6", "h-6"])} />
                 </Link>
                 <button
@@ -65,7 +126,12 @@ function Header() {
                         ["flex", "items-center", "justify-center", "p-2"],
                         ["rounded-full", "hover:bg-text-default/10"],
                     )}
-                    title={theme === "dark" ? "라이트 모드로 변경" : "다크 모드로 변경"}>
+                    title={
+                        theme === "dark"
+                            ? "라이트 모드로 변경"
+                            : "다크 모드로 변경"
+                    }
+                >
                     {theme === "dark" ? (
                         <MdLightMode className={twMerge(["w-6", "h-6"])} />
                     ) : (
@@ -73,12 +139,14 @@ function Header() {
                     )}
                 </button>
                 <Link
+                    onClick={handleUploadClick}
                     to={"/upload"}
                     className={twMerge(
                         ["flex", "items-center", "justify-center", "p-2"],
                         ["rounded-full", "hover:bg-text-default/10"],
                     )}
-                    title={"동영상 업로드"}>
+                    title={"동영상 업로드"}
+                >
                     <MdVideoCall className={twMerge(["w-6", "h-6"])} />
                 </Link>
                 <Link
@@ -86,8 +154,13 @@ function Header() {
                     className={twMerge(
                         ["flex", "items-center", "gap-2", "px-4", "py-2"],
                         ["border", "border-divider", "rounded-full"],
-                        ["text-secondary-main", "font-medium", "hover:bg-secondary-main/10"],
-                    )}>
+                        [
+                            "text-secondary-main",
+                            "font-medium",
+                            "hover:bg-secondary-main/10",
+                        ],
+                    )}
+                >
                     <FaRegUserCircle className={twMerge(["w-5", "h-5"])} />
                     <span className={twMerge("text-sm")}>로그인</span>
                 </Link>
--- src/components/ui/GlobalModal.tsx
+++ src/components/ui/GlobalModal.tsx
@@ -3,6 +3,7 @@ import { twMerge } from "tailwind-merge";
 import Backdrop from "./Backdrop.tsx";
 import { MdClose } from "react-icons/md";
 import AddressSearchModal from "./modals/AddressSearchModal.tsx";
+import LoginRequiredModal from "./modals/LoginRequiredModal.tsx";
 
 function GlobalModal() {
     const { isOpen, type, props, closeModal } = useModalStore();
@@ -32,6 +33,7 @@ function GlobalModal() {
                 </button>
 
                 {type === "ADDRESS_SEARCH" && <AddressSearchModal props={props} onClose={closeModal}/>}
+                {type === "LOGIN_REQUIRED" && <LoginRequiredModal onClose={closeModal} />}
             </div>
         </div>
     );
