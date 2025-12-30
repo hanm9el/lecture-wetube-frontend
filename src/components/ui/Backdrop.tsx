dbbaa8a23b86766b3ddc47d4ce7dc075f48a7418
HYUN <hanm9el@naver.com>
2025-12-30 15:46:22 +0900

    Header 펼침메뉴 작성 완료

--- src/components/ui/Backdrop.tsx
+++ src/components/ui/Backdrop.tsx
@@ -2,19 +2,23 @@ import { twMerge } from "tailwind-merge";
 
 type BackdropProps = {
     onClose: () => void;
+    className?: string;
 };
 
-function Backdrop({ onClose }: BackdropProps) {
+function Backdrop({ onClose, className }: BackdropProps) {
     return (
         <div
             onClick={onClose}
-            className={twMerge([
-                "fixed",
-                "inset-0",
-                "cursor-default",
-                "bg-black/60",
-                "backdrop-blur-sm",
-            ])}
+            className={twMerge(
+                [
+                    "fixed",
+                    "inset-0",
+                    "cursor-default",
+                    "bg-black/60",
+                    "backdrop-blur-sm",
+                ],
+                className,
+            )}
         />
     );
 }
