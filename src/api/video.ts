3d0fa16481e371c344ce838eaabe6e56794785f4
HYUN <hanm9el@naver.com>
2026-01-02 16:56:57 +0900

    /videos/detal구독 좋아요 완성

--- src/api/video.ts
+++ src/api/video.ts
@@ -1,36 +1,35 @@
-import { api } from "./axios.ts";
+import {api} from "./axios.ts";
 
-export interface Video {
-    id: string;
-    createdAt: string;
+export interface Video{
+    id:number;
+    createdAt:string;
 
-    title: string;
-    description: string;
-    videoPath: string;
-    thumbnailPath: string;
-    views: number;
-    likeCount: number;
-    isLiked: boolean; // 지금 보고 있는 사용자가 좋아요를 눌렀는가?
-    isSubscribed: boolean; // 지금 보고 있는 사용자가 구독을 눌렀는가?
-    subscriberCount: number; // 지금 영상의 채널장 구독자 수
-    author: {
-        id: number;
-        nickname: string;
-        profileImage?: string;
-    };
+    title:string;
+    description:string;
+    videoPath:string;
+    thumbnailPath:string;
+    views:number;
+    likeCount:number;
+    isLiked?:boolean;
+    isSubscribed?:boolean;
+    subscriberCount?:number;
+    author:{
+        id:number;
+        nickname:string;
+        profileImage?:string;
+    }
 }
-
 export const fetchVideos = async () => {
     const response = await api.get<Video[]>("/videos");
     return response.data;
-};
+}
 
-export const fetchVideo = async (videoId: number) => {
-    const response = await api.get<Video>(`/videos/${videoId}`);
+export const fetchVideo = async (videoId:number) => {
+    const response = await  api.get<Video>(`/videos/${videoId}`);
     return response.data;
-};
+}
 
-export const toggleVideoLike = async (videoId: number) => {
-    const response = await api.post<{ isLiked: boolean }>(`/videos/${videoId}/like`);
+export const toggleVideoLike = async (videoId:number) => {
+    const response = await api.post<{isLiked:boolean}>(`/videos/${videoId}/like`);
     return response.data;
-};
+}
